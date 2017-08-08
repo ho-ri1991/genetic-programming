ad8e62999152b05a0a96d6b0773b760b016f1445
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-08-09 00:07:46 +0900

    implement function which generates arbitrary combination of types from variadic template parameter pack

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <exception>
 #include<tuple>
 #include "tree/node/node_base.hpp"
+#include <boost/type_index.hpp>
 
 class Add: public tree::node::NodeBase<int(int, int)> {
 public:
@@ -34,6 +35,61 @@ const std::type_info* const getRTTI(std::size_t n)noexcept {
 }
 
 
+template <typename T1, typename T2>
+struct type_accumulator {
+    using type1 = T1;
+    using type2 = T2;
+};
+
+template <typename ...Ts>
+struct types{
+    template <std::size_t n>
+    using type = typename std::tuple_element<n, std::tuple<Ts...>>::type;
+};
+
+template <typename...> struct join;
+
+template <template <typename...> class Tpl,
+        typename ...Args1,
+        typename ...Args2>
+struct join<Tpl<Args1...>, Tpl<Args2...>>
+{
+    typedef Tpl<Args1..., Args2...> type;
+};
+
+template <std::size_t n, typename ...> struct acc;
+
+template <std::size_t n,
+          template <typename ...> class Tpl,
+          typename T1,
+          typename T2,
+          typename ...Args1,
+          typename ...Args2,
+          typename ...Args>
+struct acc<n, Tpl<T1, Args1...>, Tpl<T2, Args2...>, Tpl<Args...>>{
+    static void func() {
+        if constexpr (n == 0){
+            std::cout<< boost::typeindex::type_id_with_cvr<Tpl<T1, Args1...>>().pretty_name() <<std::endl;
+        } else {
+//            acc<n - 1, Tpl<T2, T1, Args1...>, Tpl<Args...>, Tpl<Args...>>::func();
+            acc<n - 1, Tpl<T1, Args1..., T2>, Tpl<Args...>, Tpl<Args...>>::func();
+            if constexpr (sizeof...(Args2) > 0) {
+                acc<n, Tpl<T1, Args1...>, Tpl<Args2...>, Tpl<Args...>>::func();
+            }
+        }
+    }
+};
+
+template <std::size_t n, typename ...Ts>
+void func(){
+    if constexpr (n == 0){
+
+    }else {
+        func<n - 1, Ts...>();
+        acc<n, std::tuple<int>, std::tuple<Ts...>, std::tuple<Ts...>>::func();
+    }
+}
+
 int main() {
     auto add = std::make_shared<Add>();
     auto c1 = std::make_shared<Const>(1);
@@ -41,5 +97,8 @@ int main() {
     add->setChild(0, c1);
     add->setChild(1, c2);
     std::cout<<add->evaluate()<<std::endl;
+    typename join<std::tuple<int, int>, std::tuple<double, double>>::type t;
+    std::cout<<typeid(typename join<std::tuple<int, int>, std::tuple<double, double>>::type).name()<<std::endl;
+    func<7, int, double, bool>();
     return 0;
 }
