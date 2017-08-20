c739c48e851f392a61c47a651252f3c9aade910f
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-08-20 23:25:28 +0900

    add subroutine node, WIP

---
+++ include/node/subroutine_node.hpp
@@ -0,0 +1,43 @@
+#ifndef GP_NODE_SUBROUTINE_NODE
+#define GP_NODE_SUBROUTINE_NODE
+
+#include "node_base.hpp"
+#include <tuple>
+#include <unordered_map>
+
+namespace gp::node {
+    class SubroutineEntitySet {
+    private:
+        using LocalVariableTypes = std::vector<const std::type_info*>;
+        using SubroutineEntity = std::pair<std::shared_ptr<const NodeInterface>, const LocalVariableTypes>;
+        std::unordered_map<std::string, SubroutineEntity> subroutineEntities;
+    public:
+        template <typename Key>
+        decltype(auto) find(Key&& key) {return subroutineEntities.find(std::forward<Key>(key));}
+        template <typename Key>
+        decltype(auto) find(Key&& key)const {return subroutineEntities.find(std::forward<Key>(key));}
+        template <typename Key, typename Val>
+        void insert(Key&& key, Val&& val) {subroutineEntities.insert(std::forward<Key>(key), std::forward<Val>(val));}
+    };
+
+    template <typename T, typename ...Args>
+    class SubroutineNode: public NodeBase<T(Args...)> {
+    private:
+        using ThisType = SubroutineNode;
+        const SubroutineEntitySet& subroutineEntitySet;
+        const std::string name;
+    public:
+        template <typename Name>
+        SubroutineNode(Name&& nodeName, const SubroutineEntitySet& subroutineEntitySet_)
+                : subroutineEntitySet(subroutineEntitySet_)
+                , name(std::forward<Name>(nodeName)){}
+    private:
+        T evaluationDefinition(utility::EvaluationContext* evaluationContext)const override;
+    public:
+        std::string getNodeName()const override {return name;}
+        std::shared_ptr<NodeInterface> clone()const override {return std::make_shared<ThisType>(name, subroutineEntitySet);}
+        NodeType getNodeType()const noexcept override final {return NodeType::Subroutine;}
+    };
+}
+
+#endif
