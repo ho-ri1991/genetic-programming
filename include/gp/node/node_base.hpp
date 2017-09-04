8728d5dc8ae80dd200a12fc4599cb3a42c281a5e
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-09-04 21:21:48 +0900

    move helper functions to detail namespace:

--- include/gp/node/node_base.hpp
+++ include/gp/node/node_base.hpp
@@ -10,102 +10,110 @@
 #include <any>
 
 namespace gp::node{
-    template <typename T, typename ...Ts>
-    const NodeInterface::type_info& getRTTI(std::size_t n)noexcept {
-        if constexpr (sizeof...(Ts) > 0) {
-            if(n == 0)return utility::typeInfo<T>();
-            else return getRTTI<Ts...>(n - 1);
-        } else {
-            if(n != 0) return utility::typeInfo<utility::error>();
-            else return utility::typeInfo<T>();
-        }
-    }
-
-    template <std::size_t offset, typename ...Ts>
-    NodeInterface& getDynamicHelper(std::size_t n, std::tuple<Ts...>& t) {
-        if constexpr(offset < sizeof...(Ts)){
-            if(n == 0) {
-                if(std::get<offset>(t))return *std::get<offset>(t);
-                else throw std::runtime_error("the specified child is nullptr");
-            }
-            else return getDynamicHelper<offset + 1, Ts...>(n - 1, t);
-        }else {
-            throw std::invalid_argument("index exceeds the number of children");
-        }
-    }
-
-    template <typename ...Ts>
-    NodeInterface& getDynamic(std::size_t n, std::tuple<Ts...>& t) {
-        return getDynamicHelper<0, Ts...>(n, t);
-    }
-
-    template <typename ...Ts>
-    const NodeInterface& getDynamic(std::size_t n, const std::tuple<Ts...>& t) {
-        return getDynamicHelper<0, Ts...>(n, const_cast<std::tuple<Ts...>&>(t));
-    }
-
-    template <std::size_t offset, typename ...Ts>
-    bool hasChildHelper(std::size_t n, const std::tuple<Ts...>& t)noexcept {
-        if constexpr(offset < sizeof...(Ts)){
-            if(n == 0) {
-                if(std::get<offset>(t))return static_cast<bool>(std::get<offset>(t));
-                else return false;
-            }
-            else return hasChildHelper<offset + 1, Ts...>(n - 1, t);
-        }else {
-            return false;
-        }
-    };
-
-    template <typename ...Ts>
-    bool hasChildHelper(std::size_t n, const std::tuple<Ts...>& t)noexcept {
-        return hasChildHelper<0, Ts...>(n, t);
-    }
-
-    template <std::size_t offset, typename ...Ts>
-    void setDynamicHelper(std::size_t n, std::unique_ptr<NodeInterface> node, std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...>& t){
-        if constexpr (offset < sizeof...(Ts)){
-            if(n == 0){
-                if(auto typed_node = dynamic_cast<typename std::tuple_element<offset, std::tuple<TypedNodeInterface<Ts>...>>::type*>(node.get())){
-                    std::get<offset>(t).reset(typed_node);
-                    node.release();
-                    return;
-                }else{
-                    throw std::invalid_argument("invalied type node was set as a child");
-                }
-            }else{
-                setDynamicHelper<offset + 1, Ts...>(n - 1, std::move(node), t);
-            }
-        } else {
-            throw std::invalid_argument("invalid child index");
-        }
-    };
-
-    template <typename ...Ts>
-    void setDynamic(std::size_t n, std::unique_ptr<NodeInterface> node, std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...>& t){
-        setDynamicHelper<0, Ts...>(n, std::move(node), t);
-    }
-
-    template <std::size_t n, typename> struct EvaluateChildrenHelper;
-
-    template <std::size_t n,
-              template <typename ...> class Tpl,
-              typename ...Ts>
-    struct EvaluateChildrenHelper<n, Tpl<Ts...>> {
-        static auto eval(const std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...>& children, utility::EvaluationContext& evaluationContext) {
-            if constexpr (sizeof...(Ts) - 1 == n) {
-                return std::make_tuple(std::get<n>(children)->evaluate(evaluationContext));
+    namespace detail {
+        template<typename T, typename ...Ts>
+        const NodeInterface::type_info &getRTTI(std::size_t n)noexcept {
+            if constexpr (sizeof...(Ts) > 0)
+            {
+                if (n == 0)return utility::typeInfo<T>();
+                else return getRTTI<Ts...>(n - 1);
             } else {
-                return std::tuple_cat(std::make_tuple(std::get<n>(children)->evaluate(evaluationContext)),
-                                        EvaluateChildrenHelper<n + 1, Tpl<Ts...>>::eval(children, evaluationContext));
+                if (n != 0) return utility::typeInfo<utility::error>();
+                else return utility::typeInfo<T>();
             }
         }
-    };
 
+        template<std::size_t offset, typename ...Ts>
+        NodeInterface &getDynamicHelper(std::size_t n, std::tuple<Ts...> &t) {
+            if constexpr(offset < sizeof...(Ts))
+            {
+                if (n == 0) {
+                    if (std::get<offset>(t))return *std::get<offset>(t);
+                    else throw std::runtime_error("the specified child is nullptr");
+                } else return getDynamicHelper<offset + 1, Ts...>(n - 1, t);
+            }else {
+                throw std::invalid_argument("index exceeds the number of children");
+            }
+        }
+
+        template<typename ...Ts>
+        NodeInterface &getDynamic(std::size_t n, std::tuple<Ts...> &t) {
+            return getDynamicHelper<0, Ts...>(n, t);
+        }
+
+        template<typename ...Ts>
+        const NodeInterface &getDynamic(std::size_t n, const std::tuple<Ts...> &t) {
+            return getDynamicHelper<0, Ts...>(n, const_cast<std::tuple<Ts...> &>(t));
+        }
+
+        template<std::size_t offset, typename ...Ts>
+        bool hasChildHelper(std::size_t n, const std::tuple<Ts...> &t)noexcept {
+            if constexpr(offset < sizeof...(Ts))
+            {
+                if (n == 0) {
+                    if (std::get<offset>(t))return static_cast<bool>(std::get<offset>(t));
+                    else return false;
+                } else return hasChildHelper<offset + 1, Ts...>(n - 1, t);
+            }else {
+                return false;
+            }
+        };
+
+        template<typename ...Ts>
+        bool hasChildHelper(std::size_t n, const std::tuple<Ts...> &t)noexcept {
+            return hasChildHelper<0, Ts...>(n, t);
+        }
+
+        template<std::size_t offset, typename ...Ts>
+        void setDynamicHelper(std::size_t n, std::unique_ptr<NodeInterface> node,
+                              std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...> &t) {
+            if constexpr (offset < sizeof...(Ts))
+            {
+                if (n == 0) {
+                    if (auto typed_node = dynamic_cast<typename std::tuple_element<offset, std::tuple<TypedNodeInterface<Ts>...>>::type *>(node.get())) {
+                        std::get<offset>(t).reset(typed_node);
+                        node.release();
+                        return;
+                    } else {
+                        throw std::invalid_argument("invalied type node was set as a child");
+                    }
+                } else {
+                    setDynamicHelper<offset + 1, Ts...>(n - 1, std::move(node), t);
+                }
+            } else {
+                throw std::invalid_argument("invalid child index");
+            }
+        };
+
+        template<typename ...Ts>
+        void setDynamic(std::size_t n, std::unique_ptr<NodeInterface> node,
+                        std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...> &t) {
+            setDynamicHelper<0, Ts...>(n, std::move(node), t);
+        }
+
+        template<std::size_t n, typename>
+        struct EvaluateChildrenHelper;
+
+        template<std::size_t n,
+                template<typename ...> class Tpl,
+                typename ...Ts>
+        struct EvaluateChildrenHelper<n, Tpl<Ts...>> {
+            static auto eval(const std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...> &children,
+                             utility::EvaluationContext &evaluationContext) {
+                if constexpr (sizeof...(Ts) - 1 == n)
+                {
+                    return std::make_tuple(std::get<n>(children)->evaluate(evaluationContext));
+                } else {
+                    return std::tuple_cat(std::make_tuple(std::get<n>(children)->evaluate(evaluationContext)),
+                                          EvaluateChildrenHelper<n + 1, Tpl<Ts...>>::eval(children, evaluationContext));
+                }
+            }
+        };
+    }
 
     template <typename ...Ts>
     std::tuple<Ts...> evaluateChildren(const std::tuple<std::unique_ptr<TypedNodeInterface<Ts>>...>& children, utility::EvaluationContext& evaluationContext) {
-        return EvaluateChildrenHelper<0, std::tuple<Ts...>>::eval(children, evaluationContext);
+        return detail::EvaluateChildrenHelper<0, std::tuple<Ts...>>::eval(children, evaluationContext);
     }
 
     template <typename T>
@@ -124,20 +132,20 @@ namespace gp::node{
     public:
         std::size_t getChildNum()const noexcept override {return std::tuple_size<decltype(children)>::value;}
         const type_info& getChildReturnType(std::size_t n)const noexcept override {
-            return getRTTI<Args...>(n);
+            return detail::getRTTI<Args...>(n);
         }
         NodeInterface& getChildNode(std::size_t n)override {
             assert(n < sizeof...(Args));
-            return getDynamic(n, children);
+            return detail::getDynamic(n, children);
         }
         const NodeInterface& getChildNode(std::size_t n)const override {
             assert(n < sizeof...(Args));
-            return getDynamic(n, children);
+            return detail::getDynamic(n, children);
         }
         void setChild(std::size_t n, std::unique_ptr<NodeInterface> node)override {
             assert((n < sizeof...(Args)) && "the child index must be smaller than the number of children of the node");
-            assert(getRTTI<Args...>(n) == node->getReturnType() && "the return type of child must equal to the argument type of the node");
-            setDynamic(n, std::move(node), children);
+            assert(detail::getRTTI<Args...>(n) == node->getReturnType() && "the return type of child must equal to the argument type of the node");
+            detail::setDynamic(n, std::move(node), children);
             getChildNode(n).setParent(this);
         }
         NodeInterface& getParent()override {
@@ -151,7 +159,7 @@ namespace gp::node{
         virtual NodeType getNodeType()const noexcept override {return NodeType::Normal;}
         bool hasChild(std::size_t n)const noexcept override {
             if(sizeof...(Args) <= n) return false;
-            return hasChildHelper(n, children);
+            return detail::hasChildHelper(n, children);
         }
         bool hasParent()const noexcept override {return parent != nullptr;}
     public:
