4e76785ba838127ec24a2f4d27afaf42a0e5720c
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-09-13 22:44:59 +0900

    test of recursion

--- src/main.cpp
+++ src/main.cpp
@@ -22,51 +22,41 @@ int main() {
     tree::SubroutineIO<int> subroutineIO;
 
     node::StringToNode stringToNode;
-    auto subtruct = std::make_unique<node::SubtractNode<int>>();
-    auto mult = std::make_unique<node::MultiplyNode<int>>();
-    auto progn = std::make_unique<node::PrognNode<int, 2>>();
-    auto add = std::make_unique<node::AddNode<int>>();
-    auto add1 = std::make_unique<node::AddNode<int>>();
-    auto subst = std::make_unique<node::SubstitutionNode<int>>();
-    auto lovalVar = std::make_unique<node::LocalVariableNode<int, 0>>();
-    auto localVarL = std::make_unique<node::LocalVariableNode<utility::LeftHandValue<int>, 0>>();
-    auto a1 = std::make_unique<node::ArgumentNode<int, 0>>();
-    auto a2 = std::make_unique<node::ArgumentNode<int, 1>>();
-    auto c1 = std::make_unique<node::ConstNode<int>>(10);
 
-
-    stringToNode.registerNode(subtruct->clone());
-    stringToNode.registerNode(mult->clone());
-    stringToNode.registerNode(progn->clone());
-    stringToNode.registerNode(add->clone());
-    stringToNode.registerNode(subst->clone());
-    stringToNode.registerNode(localVarL->clone());
-    stringToNode.registerNode(lovalVar->clone());
-    stringToNode.registerNode(a1->clone());
-    stringToNode.registerNode(a2->clone());
-    stringToNode.registerNode(c1->clone());
+    stringToNode.registerNode(std::make_unique<node::PrognNode<int,2>>());
+    stringToNode.registerNode(std::make_unique<node::AddNode<int>>());
+    stringToNode.registerNode(std::make_unique<node::SubstitutionNode<int>>());
+    stringToNode.registerNode(std::make_unique<node::LocalVariableNode<int,0>>());
+    stringToNode.registerNode(std::make_unique<node::LocalVariableNode<utility::LeftHandValue<int>,0>>());
+    stringToNode.registerNode(std::make_unique<node::ArgumentNode<int,0>>());
+    stringToNode.registerNode(std::make_unique<node::ArgumentNode<int,1>>());
+    stringToNode.registerNode(std::make_unique<node::ConstNode<int>>(0));
     stringToNode.registerNode(std::make_unique<node::GreaterNode<int>>());
     stringToNode.registerNode(std::make_unique<node::GreaterEqNode<int>>());
     stringToNode.registerNode(std::make_unique<node::LessThanEqNode<int>>());
     stringToNode.registerNode(std::make_unique<node::LessThanNode<int>>());
     stringToNode.registerNode(std::make_unique<node::EqualNode<int>>());
     stringToNode.registerNode(std::make_unique<node::NotEqualNode<int>>());
+    stringToNode.registerNode(std::make_unique<node::IfNode<int>>());
+    stringToNode.registerNode(std::make_unique<node::MultiplyNode<int>>());
+    stringToNode.registerNode(std::make_unique<node::SubtractNode<int>>());
 
-    std::ifstream fin("tree.xml");
+    std::ifstream fin("Fact[int].xml");
     auto treeProperty = subroutineIO.load(fin, stringToType, stringToNode);
 
     auto rootNode = stringToNode(treeProperty.name);
 
-    rootNode->setChild(0, a1->clone());
-    rootNode->setChild(1, a2->clone());
+    rootNode->setChild(0, std::make_unique<node::ArgumentNode<int,0>>());
 
-    std::ofstream fout1("tree_.xml");
+    std::ofstream fout1("Fact[int]_.xml");
     subroutineIO.write(*rootNode, treeProperty, fout1);
     fout1.close();
 
-    tree::Tree tree(typeid(int), std::vector<const std::type_info*>{&typeid(int), &typeid(int)}, std::vector<const std::type_info*>{&typeid(int)}, std::move(rootNode));
-    auto ans = tree.evaluate(std::vector<utility::Variable>{1, 2});
-    std::cout<<std::any_cast<int>(ans.getReturnValue())<<std::endl;
+    tree::Tree tree(tree::TreeProperty{&utility::typeInfo<int>(), {&utility::typeInfo<int>()}, {}, ""}, std::move(rootNode));
+    for(int i = 0; i < 10; ++i) {
+        auto ans = tree.evaluate(std::vector<utility::Variable>{i});
+        std::cout << std::any_cast<int>(ans.getReturnValue()) << std::endl;
+    }
 
     std::ofstream fout("tree.txt");
     tree_operations::writeTree(tree.getRootNode(), fout);
