719831c3633d07dd92f92a698bcf63a3327f27d8
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-09-16 13:51:24 +0900

    test of reference passing

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,10 @@ int main() {
     utility::StringToType stringToType;
     stringToType.setTypeNamePair<int>("int");
     stringToType.setTypeNamePair<utility::LeftHandValue<int>>("lvalue[int]");
+    stringToType.setTypeNamePair<utility::Reference<int>>("ref[int]");
 
     tree::TreeIO<int> treeIO;
+
 
     treeIO.registerNode(std::make_unique<node::PrognNode<int,2>>());
     treeIO.registerNode(std::make_unique<node::AddNode<int>>());
@@ -21,6 +23,8 @@ int main() {
     treeIO.registerNode(std::make_unique<node::LocalVariableNode<utility::LeftHandValue<int>,0>>());
     treeIO.registerNode(std::make_unique<node::ArgumentNode<int,0>>());
     treeIO.registerNode(std::make_unique<node::ArgumentNode<int,1>>());
+    treeIO.registerNode(std::make_unique<node::ArgumentNode<utility::Reference<int>, 0>>());
+    treeIO.registerNode(std::make_unique<node::ArgumentNode<utility::LeftHandValue<int>, 0>>());
     treeIO.registerNode(std::make_unique<node::ConstNode<int>>(0));
     treeIO.registerNode(std::make_unique<node::GreaterNode<int>>());
     treeIO.registerNode(std::make_unique<node::GreaterEqNode<int>>());
@@ -32,21 +36,18 @@ int main() {
     treeIO.registerNode(std::make_unique<node::MultiplyNode<int>>());
     treeIO.registerNode(std::make_unique<node::SubtractNode<int>>());
 
-    std::ifstream fin("tree.xml");
-    auto tree = treeIO.readTree(fin, stringToType);
-    std::ofstream fout1("treeOut.xml");
-    treeIO.writeTree(tree, fout1);
-    fout1.close();
-    for(int i = 0; i < 10; ++i) {
-        auto ans = tree.evaluate(std::vector<utility::Variable>{i, i});
-        std::cout << std::any_cast<int>(ans.getReturnValue()) << std::endl;
+    {
+        std::ifstream fin("ReferenceTestSub.xml");
+        auto treeProperty = treeIO.loadSubroutine(fin, stringToType);
     }
 
-    std::ofstream fout("tree.txt");
-    tree_operations::writeTree(tree.getRootNode(), fout);
+    std::ifstream fin("ReferenceTest.xml");
+    auto tree = treeIO.readTree(fin, stringToType);
 
-    std::cout<<tree_operations::getHeight(tree.getRootNode())<<std::endl;
-    std::cout<<tree_operations::getDepth(tree.getRootNode())<<std::endl;
+    for(int i = 0; i < 10; ++i) {
+        auto ans = tree.evaluate(std::vector<utility::Variable>{i});
+        std::cout << std::any_cast<int>(ans.getReturnValue()) << std::endl;
+    }
 
     return 0;
 }
