9521ce5fdfc531b3a4c44a552f9203675982e4a3
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-09-18 21:50:45 +0900

    test of generateTreeRandom

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <gp/tree/tree.hpp>
 #include <boost/type_index.hpp>
 #include <gp/node/node.hpp>
@@ -7,14 +8,23 @@
 #include <gp/tree/io.hpp>
 using namespace gp;
 
+using rnd = std::mt19937;
+
 int main() {
     utility::StringToType stringToType;
     stringToType.setTypeNamePair<int>("int");
     stringToType.setTypeNamePair<utility::LeftHandValue<int>>("lvalue[int]");
     stringToType.setTypeNamePair<utility::Reference<int>>("ref[int]");
+    stringToType.setTypeNamePair<bool>("bool");
+
+    rnd Rnd(0);
+    std::tuple<std::function<int(rnd&)>, std::function<bool(rnd&)>> random_gens = std::make_tuple(
+            std::function<int(rnd&)>([](rnd& RND){return std::uniform_int_distribution<int>(0, 10)(RND);}),
+            std::function<bool(rnd&)>([](rnd& RND){return static_cast<bool>(std::uniform_int_distribution<int>(0, 1)(RND));})
+    );
 
     tree::TreeIO<int> treeIO;
-
+    tree::RandomNodeGenerator<rnd, int, bool> randomNodeGenerator(random_gens);
 
     treeIO.registerNode(std::make_unique<node::PrognNode<int,2>>());
     treeIO.registerNode(std::make_unique<node::AddNode<int>>());
@@ -35,6 +45,42 @@ int main() {
     treeIO.registerNode(std::make_unique<node::IfNode<int>>());
     treeIO.registerNode(std::make_unique<node::MultiplyNode<int>>());
     treeIO.registerNode(std::make_unique<node::SubtractNode<int>>());
+    treeIO.registerNode(std::make_unique<node::ConstNode<bool>>(false));
+
+    randomNodeGenerator.registerNode(std::make_unique<node::PrognNode<int,2>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::AddNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::SubstitutionNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::LocalVariableNode<int,0>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::LocalVariableNode<utility::LeftHandValue<int>,0>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::ArgumentNode<int,0>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::ArgumentNode<int,1>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::ArgumentNode<utility::Reference<int>, 0>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::ArgumentNode<utility::LeftHandValue<int>, 0>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::ConstNode<int>>(0));
+    randomNodeGenerator.registerNode(std::make_unique<node::GreaterNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::GreaterEqNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::LessThanEqNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::LessThanNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::EqualNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::NotEqualNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::IfNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::MultiplyNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::SubtractNode<int>>());
+    randomNodeGenerator.registerNode(std::make_unique<node::ConstNode<bool>>(false));
+
+    for(int i=0;i<10;++i) {
+
+        auto treeProperty = tree::TreeProperty{&utility::typeInfo<int>(), {&utility::typeInfo<int>()}, {}, "RandomTree"};
+        std::cout << "generate tree randomly" << std::endl;
+        auto randomTree = tree_operations::generateTreeRandom(treeProperty, randomNodeGenerator, Rnd, 10);
+
+        {
+            std::ofstream fout("RandomTree" + std::to_string(i) + ".xml");
+            std::cout << "write random tree" << std::endl;
+            treeIO.writeTree(*randomTree, treeProperty, fout);
+        }
+    }
+
 
     {
         std::ifstream fin("ReferenceTestSub.xml");
