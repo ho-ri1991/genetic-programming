899f7edcfe16213abac7d1628fbe955ecbbfc244
ho-ri1991 <hage.1991@hotmail.co.jp>
2017-11-10 22:34:46 +0900

    change the way of loading tree

--- example1/tree_executor.cpp
+++ example1/tree_executor.cpp
@@ -59,110 +59,149 @@ int main(){
     gpManager.registerNode<node::RepeatNode<bool, int>>();
     gpManager.registerNode<node::RepeatNode<double, int>>();
 
-    while(true) {
-        std::cout << "Write tree file name or \'q\' and press Enter to exit." << std::endl;
-        std::string input;
-        std::cin >> input;
-        if(input == "q") {
-            std::cout << "Bye!" << std::endl;
-            break;
-        }
-        std::ifstream fin(input);
-        if(!fin.is_open()) {
-            std::cout << "Failed to open file, check file name" << std::endl;
-            continue;
-        }
-        try {
-            auto treeResult  = gpManager.readTree(fin);
-            if(!treeResult) {
-                std::cout << "Failed to load tree, " << treeResult.errMessage() << std::endl;
-                continue;
-            }
-            auto tree = std::move(treeResult).unwrap();
-            auto anyToStringResult = anyToStrings.find(utility::TypeIndex(tree.getReturnType())) != std::end(anyToStrings)
-                                     ? utility::result::ok(std::ref(anyToStrings.find(utility::TypeIndex(tree.getReturnType()))->second))
-                                     : utility::result::err<std::reference_wrapper<AnyToString>>("failed to load tree, unknown return type: " + tree.getReturnType().name());
-
-            auto stringToValueResult = [&stringToVariables, &prop = tree.getTreeProperty()](){
-                std::vector<utility::Result<StringToVariable>> ans;
-                ans.reserve(std::size(prop.argumentTypes));
-                std::transform(std::begin(prop.argumentTypes),
-                               std::end(prop.argumentTypes),
-                               std::back_inserter(ans),
-                               [&stringToVariables](const utility::TypeInfo* x){
-                                   if(!x) return utility::result::err<StringToVariable>("failed to load tree, argument type was not set properly");
-                                   auto itr = stringToVariables.find(utility::TypeIndex(*x));
-                                   return itr != std::end(stringToVariables)
-                                          ? utility::result::ok(itr->second)
-                                          : utility::result::err<StringToVariable>("failed to load tree, unknown argument type: " + x->name());
-                               });
-                return utility::result::sequence(std::move(ans));
-            }();
-
-            if(!anyToStringResult || !stringToValueResult) {
-                std::cout << utility::result::sequence(anyToStringResult, stringToValueResult).errMessage() << std::endl;
-                continue;
-            }
-
-            AnyToString& anyToString = anyToStringResult.unwrap();
-            auto& stringToValue = stringToValueResult.unwrap();
-
-            std::cout << "tree name: " << tree.getTreeProperty().name << '\n';
-            std::cout << "return type: " << tree.getTreeProperty().returnType->name() << '\n';
-            std::cout << "argument types: ";
-            for(auto typePtr: tree.getTreeProperty().argumentTypes)
-                std::cout << typePtr->name() << ',';
-            std::cout << std::endl;
-            {
-                //dump remaining str
-                std::string tmp;
-                std::getline(std::cin, tmp);
-            }
-
-            while(true) {
-                std::cout << "input arguments separated by space to evaluate tree or press \'q\' and enter to quit evaluation" << std::endl;
-                std::string in;
-                std::getline(std::cin, in);
-                if(in == "q") {
-                    std::cout << "quit evaluation of the tree \"" << tree.getTreeProperty().name << "\"" << std::endl;
-                    break;
-                }
-                std::istringstream iss(in);
-                std::vector<std::string> argStrs;
-                argStrs.reserve(tree.getArgumentNum());
-                std::copy(std::istream_iterator<std::string>(iss),
-                          std::istream_iterator<std::string>(),
-                          std::back_inserter(argStrs));
-                auto argsResult = (std::size(argStrs) == tree.getArgumentNum()
-                                   ? utility::result::ok(std::move(argStrs))
-                                   : utility::result::err<std::vector<std::string>>("the number of argument is invalid"))
-                        .flatMap([&stringToValue](auto&& argStrings){
-                            int n = 0;
-                            std::vector<utility::Result<utility::Variable>> ans;
-                            ans.reserve(std::size(argStrings));
-                            std::transform(std::begin(argStrings),
-                                           std::end(argStrings),
-                                           std::back_inserter(ans),
-                                           [&n, &stringToValue](const std::string& str){
-                                               return utility::result::tryFunction(
-                                                       [&n, &stringToValue, &str](){return stringToValue[n++](str);},
-                                                       "failed to convert string to value");
-                                           });
-                            return utility::result::sequence(ans);
-                        });
-                if(!argsResult) {
-                    std::cout << argsResult.errMessage() << std::endl;
+    try {
+        while (true) {
+            std::cout << "\'l\': load tree from file\n";
+            std::cout << "\'e\': execute loaded tree\n";
+            std::cout << "\'q\': exit" << std::endl;
+            std::string input;
+            std::cin >> input;
+            if (input == "q") {
+                std::cout << "Bye!" << std::endl;
+                break;
+            } else if (input == "l") {
+                std::cout << "Write file name" << std::endl;
+                std::string fileName;
+                std::cin >> fileName;
+                std::ifstream fin(std::move(fileName));
+                if (!fin.is_open()) {
+                    std::cout << "Failed to open file, check file name" << std::endl;
                     continue;
                 }
-                auto evaluatedValue = tree.evaluate(std::move(argsResult).unwrap());
-                if(evaluatedValue.getEvaluationStatus() != utility::EvaluationStatus::ValueReturned) {
-                    std::cout << "failed to evaluate tree: " << static_cast<int>(evaluatedValue.getEvaluationStatus()) << std::endl;
+                std::cout << "loading" << std::endl;
+                auto subroutineResult = gpManager.loadSubroutine(fin);
+                std::cout << "loaeded" << std::endl;
+                if (!subroutineResult) {
+                    std::cout << subroutineResult.errMessage() << std::endl;
                     continue;
                 }
-                std::cout << "the evaluated value is: " << anyToString(evaluatedValue.getReturnValue()) << std::endl;
+                auto & [subroutineNode, treeProperty] = subroutineResult.unwrap();
+                std::cout << subroutineNode->getNodeName() << " is loaded" << std::endl;
+            } else if (input == "e") {
+                std::cout << "Write tree name" << std::endl;
+                std::string name;
+                std::cin >> name;
+                auto treeResult = gpManager.getSubroutineAsTree(std::move(name));
+                if (!treeResult) {
+                    std::cout << "tree not found, check tree name" << std::endl;
+                    continue;
+                }
+                auto tree = std::move(*treeResult);
+                auto anyToStringResult =
+                        anyToStrings.find(utility::TypeIndex(tree.getReturnType())) != std::end(anyToStrings)
+                        ? utility::result::ok(
+                                std::ref(anyToStrings.find(utility::TypeIndex(tree.getReturnType()))->second))
+                        : utility::result::err<std::reference_wrapper<AnyToString>>(
+                                "failed to load tree, unknown return type: " + tree.getReturnType().name());
+
+                auto stringToValueResult = [&stringToVariables, &prop = tree.getTreeProperty()]() {
+                    std::vector<utility::Result<StringToVariable>> ans;
+                    ans.reserve(std::size(prop.argumentTypes));
+                    std::transform(std::begin(prop.argumentTypes),
+                                   std::end(prop.argumentTypes),
+                                   std::back_inserter(ans),
+                                   [&stringToVariables](const utility::TypeInfo *x) {
+                                       if (!x)
+                                           return utility::result::err<StringToVariable>(
+                                                   "failed to load tree, argument type was not set properly");
+                                       auto itr = stringToVariables.find(utility::TypeIndex(*x));
+                                       return itr != std::end(stringToVariables)
+                                              ? utility::result::ok(itr->second)
+                                              : utility::result::err<StringToVariable>(
+                                                       "failed to load tree, unknown argument type: " + x->name());
+                                   });
+                    return utility::result::sequence(std::move(ans));
+                }();
+
+                if (!anyToStringResult || !stringToValueResult) {
+                    std::cout << utility::result::sequence(anyToStringResult, stringToValueResult).errMessage()
+                              << std::endl;
+                    continue;
+                }
+
+                AnyToString &anyToString = anyToStringResult.unwrap();
+                auto &stringToValue = stringToValueResult.unwrap();
+
+                std::cout << "tree name: " << tree.getTreeProperty().name << '\n';
+                std::cout << "return type: " << tree.getTreeProperty().returnType->name() << '\n';
+                std::cout << "argument types: ";
+                for (auto typePtr: tree.getTreeProperty().argumentTypes)
+                    std::cout << typePtr->name() << ',';
+                std::cout << std::endl;
+                {
+                    //dump remaining str
+                    std::string tmp;
+                    std::getline(std::cin, tmp);
+                }
+
+                while (true) {
+                    std::cout << "input arguments separated by space to evaluate tree or press \'q\' and enter to quit evaluation"
+                              << std::endl;
+                    std::string in;
+                    std::getline(std::cin, in);
+                    if (in == "q") {
+                        std::cout << "quit evaluation of the tree \"" << tree.getTreeProperty().name << "\""
+                                  << std::endl;
+                        break;
+                    }
+                    std::istringstream iss(in);
+                    std::vector<std::string> argStrs;
+                    argStrs.reserve(tree.getArgumentNum());
+                    std::copy(std::istream_iterator<std::string>(iss),
+                              std::istream_iterator<std::string>(),
+                              std::back_inserter(argStrs));
+                    auto argsResult = (std::size(argStrs) == tree.getArgumentNum()
+                                       ? utility::result::ok(std::move(argStrs))
+                                       : utility::result::err<std::vector<std::string>>(
+                                    "the number of argument is invalid"))
+                            .flatMap([&stringToValue](auto &&argStrings) {
+                                int n = 0;
+                                std::vector<utility::Result<utility::Variable>> ans;
+                                ans.reserve(std::size(argStrings));
+                                std::transform(std::begin(argStrings),
+                                               std::end(argStrings),
+                                               std::back_inserter(ans),
+                                               [&n, &stringToValue](const std::string &str) {
+                                                   return utility::result::tryFunction(
+                                                           [&n, &stringToValue, &str]() {
+                                                               return stringToValue[n++](str);
+                                                           },
+                                                           "failed to convert string to value");
+                                               });
+                                return utility::result::sequence(ans);
+                            });
+                    if (!argsResult) {
+                        std::cout << argsResult.errMessage() << std::endl;
+                        continue;
+                    }
+                    auto evaluatedValue = tree.evaluate(std::move(argsResult).unwrap());
+                    if (evaluatedValue.getEvaluationStatus() != utility::EvaluationStatus::ValueReturned) {
+                        std::cout << "failed to evaluate tree: "
+                                  << static_cast<int>(evaluatedValue.getEvaluationStatus())
+                                  << std::endl;
+                        continue;
+                    }
+                    std::cout << "the evaluated value is: " << anyToString(evaluatedValue.getReturnValue()) << std::endl;
+                }
+            } else {
+                std::cout << "unkown command" << std::endl;
             }
-        } catch (std::exception& ex) {
-            std::cout << "failed to load tree, " << ex.what() << std::endl;
         }
+    } catch (std::exception& ex) {
+        std::cout << "unexpected error occured, " << ex.what() <<std::endl;
+    } catch (...) {
+        std::cout <<  "unexpected error occured" <<std::endl;
     }
+
+    return 0;
 }
